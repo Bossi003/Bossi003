5e7de8e85c81dbaddd9ad47e7a530c5a02aef2f6
Bossi003 <89210837+Bossi003@users.noreply.github.com>
2022-12-05 00:56:01 +0100

    Update and rename linktree.html to app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+//ist eine funktion damit man die weiterleitung ins clipboard kopiert !
+
+const sharebuttons = document.querySelectorAll('.tile-share-button')
+console.log(sharebuttons)
+
+async function copytext(e){
+//damit der button nicht zur seite geht (wegen der await methode unten muss es eine async function sein)
+  e.preventDefault()
+  const link = this.getAttribute('link')
+  console.log(link)  
+  try {
+     await navigator.clipboard.writeText(link)
+        alert("Copied to Clipboard " + link)
+  } catch (err) {
+    console.error(err)
+  }
+}
+sharebuttons.forEach(sharebutton => sharebutton.addEventListener('click', copytext))
